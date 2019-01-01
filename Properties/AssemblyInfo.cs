51614e368914cc6515827cb8dc7430cdd3b8e34a
Oliver Kind <github@oliver-kind.de>
2019-01-01 13:01:41 +0100

    Updated to File Version 3.1.1.8

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("3.1.0.7")]
-[assembly: AssemblyFileVersion("3.1.0.7")]
+[assembly: AssemblyVersion("3.1.1.8")]
+[assembly: AssemblyFileVersion("3.1.1.8")]
 [assembly: Guid ("90558fe2-53a8-410f-9544-d9de861732fb")]
