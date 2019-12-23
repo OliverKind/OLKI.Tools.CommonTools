f519d26846e28db393a3ffdabaaf1931df4bca10
Oliver Kind <github@oliver-kind.de>
2019-12-24 00:01:12 +0100

    Updated to Version 5.0.0.12

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("4.0.0.11")]
-[assembly: AssemblyFileVersion("4.0.0.11")]
+[assembly: AssemblyVersion("5.0.0.12")]
+[assembly: AssemblyFileVersion("5.0.0.12")]
 [assembly: Guid ("90558fe2-53a8-410f-9544-d9de861732fb")]
