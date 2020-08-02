9252419a9116880de22280bddb6b4150ee341cf9
Oliver Kind <github@oliver-kind.de>
2020-08-02 13:02:04 +0200

    Updated to version 7.0.0.17

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion("6.2.0.16")]
-[assembly: AssemblyFileVersion("6.2.0.16")]
+[assembly: AssemblyVersion("7.0.0.17")]
+[assembly: AssemblyFileVersion("7.0.0.17")]
 [assembly: Guid ("90558fe2-53a8-410f-9544-d9de861732fb")]
